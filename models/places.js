6fcdd1b68a6556583502834abb4ac122c33ec93f
Zachary Davis <kicksilver@icloud.com>
2022-07-31 10:05:35 -0400

    Added models/places

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/coffee-mug-in-coffee-shop.jpg'
+}, {
+    name: 'StrawHat Cafe',
+    city: 'Tokyo',
+    state: 'Japan',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/luffycoffee.jpg'
+}]
