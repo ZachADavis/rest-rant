65c7186cbf294b5cab1e612b764f8729c90610df
Zachary Davis <kicksilver@icloud.com>
2022-08-09 18:12:23 -0400

    Adjusted controller and models

--- controllers/places.js
+++ controllers/places.js
@@ -13,14 +13,65 @@ router.get('/', (req, res) => {
         })
 })
 
-// post
+// Post
 router.post('/', (req, res) => {
-    if (!req.body.pic) {
-        // Default image if one is not provided
-        req.body.pic = 'http://placekitten.com/400/400'
-    }
     db.Place.create(req.body)
         .then(() => {
+            res.redirect('/places')
+        })
+        .catch(err => {
+            if (err && err.name == 'ValidationError') {
+                let message = 'Validation Error: '
+                for (var field in err.errors) {
+                    message += `${field} was ${err.errors[field].value}. `
+                    message += `${err.errors[field].message}`
+                }
+                console.log('Validation error message', message)
+                res.render('places/new', { message })
+            }
+            else {
+                res.render('error404')
+            }
+        })
+})
+
+
+// New page
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+// Show page
+router.get('/:id', (req, res) => {
+    db.Place.findById(req.params.id)
+        .populate('comments')
+        .then(place => {
+            console.log(place.comments)
+            res.render('places/show', { place })
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
+})
+
+// For put process
+router.put('/:id', (req, res) => {
+    db.Place.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => {
+            res.redirect(`/places/${req.params.id}`)
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
+})
+
+
+// delete
+router.delete('/:id', (req, res) => {
+    db.Place.findByIdAndDelete(req.params.id)
+        .then(place => {
             res.redirect('/places')
         })
         .catch(err => {
@@ -30,46 +81,50 @@ router.post('/', (req, res) => {
 })
 
 
-// new
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
-
-// show
-router.get('/:id', (req, res) => {
+// edit page 
+router.get('/:id/edit', (req, res) => {
     db.Place.findById(req.params.id)
         .then(place => {
-            res.render('places/show', { place })
+            res.render('places/edit', { place })
         })
         .catch(err => {
-            console.log('err', err)
             res.render('error404')
         })
 })
 
 
-
-// put
-router.put('/:id', (req, res) => {
-    res.send('PUT /places/:id stub')
+// For comments page
+router.post('/:id/comment', (req, res) => {
+    console.log(req.body)
+    db.Place.findById(req.params.id)
+        .then(place => {
+            db.Comment.create(req.body)
+                .then(comment => {
+                    place.comments.push(comment.id)
+                    place.save()
+                        .then(() => {
+                            res.redirect(`/places/${req.params.id}`)
+                        })
+                })
+                .catch(err => {
+                    res.render('error404')
+                })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
 })
 
-// delete
-router.delete('/:id', (req, res) => {
-    res.send('DELETE /places/:id stub')
-})
-
-// edit
-router.get('/:id/edit', (req, res) => {
-    res.send('GET edit form stub')
-})
-
-router.post('/:id/rant', (req, res) => {
-    res.send('GET /places/:id/rant stub')
-})
-
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+// delete comments 
+router.delete('/:id/comment/:commentId', (req, res) => {
+    db.Comment.findByIdAndDelete(req.params.commentId)
+        .then(() => {
+            res.redirect(`/places/${req.params.id}`)
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
 module.exports = router
--- models/places.js
+++ models/places.js
@@ -1,4 +1,4 @@
-module.exports = [{
+/*module.exports = [{
     name: 'Starbucks',
     city: 'Virginia',
     state: 'VA',
@@ -10,4 +10,27 @@ module.exports = [{
     state: 'Japan',
     cuisines: 'Coffee, Bakery',
     pic: '/images/luffycoffee.jpg'
-}]
+}] */ 
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    pic: { type: String, default: 'http://placekitten.com/350/350' },
+    cuisines: { type: String, required: true },
+    city: { type: String, default: 'Anytown' },
+    state: { type: String, default: 'USA' },
+    founded: {
+        type: Number,
+        min: [1673, 'Surely not that old?!'],
+        max: [new Date().getFullYear(), 'This is the future!']
+    },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+})
+
+
+
+placeSchema.methods.showEstablished = function () {
+    return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+}
+
+module.exports = mongoose.model('Place', placeSchema)
